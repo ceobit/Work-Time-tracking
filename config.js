7b744f3f199186eb121a3986a6d133a051853dbf
Roman Lebedenko <ceobit90@gmail.com>
2021-03-12 15:37:49 +0100

    mongo bug

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 module.exports.PORT = PORT;
 
-module.exports.dbURI = process.env.dbURI;
+// module.exports.dbURI = process.env.dbURI;
+module.exports.dbURI = 'mongodb://localhost:27017/workTime';
 module.exports.SECRET_KEY =
   process.env.NODE_ENV === 'production' ? process.env.SECRET_KEY : 'secret key';
